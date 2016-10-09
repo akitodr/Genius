47b389948366b4693e484bee45442bb9736590a0
akitodr <lara.muller@workmail.com>
2016-10-09 04:13:04 -0300

    4 buttons

--- Genius/Genius/src/Buttons.cpp
+++ Genius/Genius/src/Buttons.cpp
@@ -1,29 +1,41 @@
 #include "Buttons.h"
 
-Button::Button(ofVec2f pos, char* notClicked, char* clicked, char* sound) {
+Button::Button(ofVec2f pos, char* notClicked, char* clicked, char* sound) { //construtor
+	//da load nas parada
 	position = pos;
 	colorNotClicked.load(notClicked);
 	colorClicked.load(clicked);
 	note.load(sound);
 }
 
-bool Button::Check(ofVec2f posMouse) {
-	return (posMouse.x > position.x && posMouse.x < (position.x + SIZEX)
+bool Button::Check(ofVec2f posMouse) {//checa colisão do mouse com o botão
+	IsClicked = (posMouse.x > position.x && posMouse.x < (position.x + SIZEX)
 		&& posMouse.y > position.y && posMouse.y < (position.y + SIZEY));
+
+	return IsClicked;
 }
 
 void Button::Play() {
 
+	note.play();
+	cout << "Não Play!" << endl;
+
+	if (!IsClicked) {
+		note.stop();
+		IsClicked = false;
+		cout << "Play" << endl;
+	}
+
 }
 
 void Button::Draw() {
-	if (!IsClicked) {
-		colorNotClicked.draw(position.x, position.y);
-		cout << "Verde" << endl;
-	}
-	else {
+
+	colorNotClicked.draw(position.x, position.y);
+	cout << "Verde" << endl;
+
+	if (IsClicked) {
 		colorClicked.draw(position.x, position.y);
-		cout << "Deu ruim!" << endl;
+		cout << "Não verde!" << endl;
 	}
 }
 
--- Genius/Genius/src/ofApp.cpp
+++ Genius/Genius/src/ofApp.cpp
@@ -6,76 +6,88 @@ vector<Button*> button;
 ofVec2f mousePos;
 
 Button* green;
+Button* red;
+Button* yellow;
+Button* blue;
 
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 	green = new Button(ofVec2f(100, 100), "img/green.png", "img/greenClick.png", "sound/do.wav");
+	red = new Button(ofVec2f(100+SIZEX+10, 100), "img/red.png", "img/redClick.png", "sound/re.wav");
+	yellow = new Button(ofVec2f(100, 100+SIZEY+10), "img/yellow.png", "img/yellowClick.png", "sound/fa.wav");
+	blue = new Button(ofVec2f(100 + SIZEX + 10, 100 + SIZEY + 10), "img/blue.png", "img/blueClick.png", "sound/mi.wav");
 	button.push_back(green);
+	button.push_back(red);
+	button.push_back(yellow);
+	button.push_back(blue);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 	green->Check(mousePos);
+	//green->Play();
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-		green->Draw();
+void ofApp::draw() {
+	green->Draw();
+	red->Draw();
+	blue->Draw();
+	yellow->Draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-	
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-	
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
 	mousePos.set(x, y);
-	green->IsClicked = true;
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
